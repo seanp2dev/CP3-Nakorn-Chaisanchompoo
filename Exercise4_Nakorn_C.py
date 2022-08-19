053f828c8509e50792f6e090a5e2bc7a1bbacd1f
seanp2dev <nynakornch@yahoo.com>
2022-08-19 10:30:27 +0700

    Create Exercise4_Nakorn_C.py

---
+++ Exercise4_Nakorn_C.py
@@ -0,0 +1,21 @@
+subject1 = "Foundation English"
+subject2 = "General Business"
+subject3 = "Introduction to Computer Systems"
+subject4 = "Computer Programming"
+
+student_id = "0001"
+student_name = "ant"
+FE = 45
+GB = 63
+CS = 87
+CP = 92
+
+print("--- Your Score ---")
+print(student_id + " " + student_name)
+print(subject1 , " : " , FE)
+print(subject2 , " : " , GB)
+print(subject3 , " : " , CS)
+print(subject4 , " : " , CP)
+
+
+
